ff270df430d1833aa73a071e0b6706dfd0a689f3
Erik Schnetter <schnetter@gmail.com>
2025-01-23 10:09:22 -0500

    Correct initialization of sparse matrix

--- src/NotDijkstra.jl
+++ src/NotDijkstra.jl
@@ -9,24 +9,25 @@ export make_graph
 function make_graph(n::Int)
     # Invent random directed graph
     Random.seed!(5)
-    A = Matrix(sprand(n, n, 2/n))
-    # Replace zeros by infinity
-    A = map(a -> ifelse(a==0, Inf, abs(a)), A)
+    A = sprand(n, n, 2/n)
+    # Convert nonzero entries to tropical numbers
+    I,J,V = findnz(A)
+    V = TropicalMinPlusF64.(V)
     # Set diagonal to zero
-    for i in 1:size(A,1)
-        A[i,i] = 0
-    end
-    # Convert to tropical matrix
-    return TropicalMinPlus.(A)
+    append!(I, 1:n)
+    append!(J, 1:n)
+    append!(V, LinRange(0, 0, n))
+    A = sparse(I, J, V)
+    return A
 end
 
 export make_initial
 function make_initial(n::Int)
     # All vertices are unreachable
-    x = fill(Inf, n)
+    x = zeros(TropicalMinPlusF64, n)
     # The first vertex is reachable with zero cost
     x[1] = 0
-    return TropicalMinPlus.(x)
+    return x
 end
 
 export find_costs
